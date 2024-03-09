dd73479fb5f2a08990fae94de4cedad72aa3bf5f
Jesus1102 <jesus60302@gmail.com>
2024-03-09 17:27:23 -0500

    Prueba

---
+++ Principal/Inicio.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "../Salud/Persona.cpp"
+
+using namespace std;
+
+class Inicio{
+    
+   
+
+
+};
+
+int main (){
+
+    Persona user1;
+
+    user1.pedirDatos();
+    user1.mostrarDatos();
+    user1.calcularImc();
+    user1.mayorEdad();
+    
+    return 0;
+}
