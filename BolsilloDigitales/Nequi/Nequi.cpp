b40fbebf61ae2132a23c1603cde551cd8f91bc03
Jesus1102 <jesus60302@gmail.com>
2024-03-09 18:15:53 -0500

    Cree accesoApp y su ciclo

--- BolsilloDigitales/Nequi/Nequi.cpp
+++ BolsilloDigitales/Nequi/Nequi.cpp
@@ -5,6 +5,33 @@ class Nequi{
 
     private:
     int opcion;
-    string claves[10] = {}, usuario[10] = {};
+    string clavesRegistradas[10] = {}, usuariosRegistrados[10] = {}, clave, usuario;
+
+    bool accesoApp(){
+
+        while(true){
+            cout << "Ingrese su usuario: " << endl;
+            cin >> usuario;
+
+            cout << "Ingrese su clave: " << endl;
+            cin >> clave;
+
+            for (int i = 0; i < 10; i++){
+                
+                if (clave == clavesRegistradas[i] && usuario == usuariosRegistrados[i]){
+                    return true;
+
+                }
+                else{
+
+                    cout << "Clave o usuario incorrestos" << endl;
+                    break;
+                }
+                
+
+            }
+            
+        };
+    }
 
 };
