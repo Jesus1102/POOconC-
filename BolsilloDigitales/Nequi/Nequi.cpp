0713a685e340f3fe4de6845370e879b534713c79
Jesus1102 <jesus60302@gmail.com>
2024-03-19 12:07:04 -0500

    Termine la funcion plata al toque

--- BolsilloDigitales/Nequi/Nequi.cpp
+++ BolsilloDigitales/Nequi/Nequi.cpp
@@ -30,14 +30,60 @@ class Nequi{
             }
             i--;
 
-            cout << "Clave o usuario incorrestos" << endl;
+            cout << "Clave o usuario incorrectos" << endl;
             cout << "Ingrese un usuario y clave validos" << endl;
             cout << "Tiene " << i << "intentos mas" << endl;
 
         };
     }
 
-    void recarga(){
+    string recarga(){
+
+        int numCuenta, cedula;
+        float valor;
+
+        cout << "ingrese la opcion que desea para recargar: " << endl;
+        cout << "1. Plata al toque" << endl;
+        cout << "2. Con efectivo" << endl;
+        cout << "3. Desde otro banco" << endl;
+        cout << "4. Codigo de regalo" << endl;
+        cout << "0. para retroceder" << endl;
+        cin >> opcion;
+        while (true)
+        {              
+            if(opcion == 1){
+                cout << "Bienvenido a Plata al toque" << endl;
+                cout << "Ingrese el numero de cuenta bancolombia: " << endl;
+                cin >> numCuenta;
+                cout << "Ingrese su numero de cedula: " << endl;
+                cin >> cedula;
+                while (true)
+                {
+                    cout << "Ingrese el valor a enviar: " << endl;
+                    cin >> valor;
+                    if(valor <= 0){
+                        cout << "Ingrese un valor valido" << endl;
+                    }
+                    else{
+                        return "Recarga realizada con exito";
+                    }
+
+                }
+
+            }
+            if(opcion == 2){
+                
+            }
+            if(opcion == 3){
+                
+            }
+            if(opcion == 4){
+                
+            }
+            if(opcion == 5){
+                
+            }
+        }
 
     }
     void colchon(){
